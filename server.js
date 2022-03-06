0a75c98d15b49bcfc15a01737c33930b4aa6561e
Dennis Hulme <dhbcarvalho@yahoo.com.br>
2022-03-06 14:45:09 +0000

    solução com a inclusão das linhas de comando no arquivo server.js

--- server.js
+++ server.js
@@ -31,3 +31,16 @@ app.get("/api/hello", function (req, res) {
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+
+//iniciando as configurações
+
+let responseObject = {}
+app.enable("trust proxy")
+
+app.get("/api/whoami", (request, response)=>{
+  responseObject["ipadress"] = request.ip
+  responseObject["language"] = request.get("Accept-Language")
+  responseObject["software"] = request.get("User-Agent")
+response.json(responseObject)
+})
